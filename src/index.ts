eaa59496671ad197a209fdf66f596057791006a9
Антон Матюшин <anton.matiushin@gmail.com>
2019-07-31 17:25:10 +0300

    1) refactor helpers: functions to static classes

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './FetchHelper';
 export * from './DomHelper';
 export * from './BrowserHelper';
 export * from './FileHelper';
+export * from './WebRtcHelper';
 export * from './type/http';
 export * from './type/type';
